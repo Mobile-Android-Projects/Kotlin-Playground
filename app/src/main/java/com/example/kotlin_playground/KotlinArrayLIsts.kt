6ff372fd48f1d7bcb1e49e451dd879932af7a0c3
Auravision <auravision.africa@gmail.com>
2021-10-20 13:09:41 +0300

    fun with Kotlin collections

--- app/src/main/java/com/example/kotlin_playground/KotlinArrayLIsts.kt
+++ app/src/main/java/com/example/kotlin_playground/KotlinArrayLIsts.kt
@@ -1,2 +1,22 @@
 package com.example.kotlin_playground
 
+fun main(){
+    //immutable array list of type double
+    val numberList = ArrayList<Double>(5)
+    numberList.add(350000.0)
+    numberList.add(700000.0)
+    numberList.add(1400000.0)
+    numberList.add(2800000.0)
+    numberList.add(5600000.0)
+
+    //value to hold the total of the numbers
+    var numberTotal:Double = 0.0
+
+    for(i in numberList)
+    {
+        numberTotal += i
+    }
+    val numberAvg = numberTotal/numberList.size
+    println("The average of the numberList is: $numberAvg")
+    println("The total of the numberList is: ${numberTotal} ")
+}
